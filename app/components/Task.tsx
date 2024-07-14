5a6ab6e2998643960a78c544c2dab09648c39b7a
LakayFTW <MilchmannLetsPlay@gmail.com>
2024-07-14 16:45:32 +0200

    Added delete and edit buttons, no functionality

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -4,7 +4,7 @@ interface Props {
 }
 export default function Task({ name, desc }: Props) {
     return (
-        <div className="flex flex-row w-full max-w-2xl px-10 py-2 rounded-md max-h-40 bg-custom-secondary">
+        <div className="flex flex-row w-full max-w-2xl px-5 pl-10 py-2 rounded-md max-h-40 bg-custom-secondary">
             <div className="flex flex-col w-1/2 space-y-2">
                 <div className="text-2xl">
                     <p className="underline underline-offset-4">{name}</p>
@@ -16,6 +16,17 @@ export default function Task({ name, desc }: Props) {
             <div className="w-1/2">
                 <p>Hier kommt die Time hin die gebraucht wurde</p>
             </div>
-        </div>
+            <div className="flex flex-row w-1/4 justify-between items-center space-x-3">
+                <div className="h-full w-1/2 flex items-center justify-center">
+                    <svg className="text-custom-text hover:scale-125 hover:-rotate-45 transition ease-in-out duration-300 hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" /></svg>
+                </div>
+                {/*  */}
+                <div className="h-full w-1/2 flex items-center justify-center">
+                    <svg className="text-custom-text -rotate-45 transition ease-in-out duration-300 hover:rotate-45 hover:scale-125 hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 24 24" fill="currentColor">
+                        <path fillRule="evenodd" d="M 11 2 L 11 11 L 2 11 L 2 13 L 11 13 L 11 22 L 13 22 L 13 13 L 22 13 L 22 11 L 13 11 L 13 2 Z"></path>
+                    </svg>
+                </div>
+            </div>
+        </div >
     )
 }
